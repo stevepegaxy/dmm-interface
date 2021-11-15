115049af5bbcc218cd06c497ae651edf8377bae7
tien7668 <tien7668@gmail.com>
2021-11-15 17:57:02 +0700

    https://levelup.gitconnected.com/publish-react-components-as-an-npm-package-7a671a2fb7f

---
+++ dist/index.js
@@ -0,0 +1,16 @@
+"use strict";
+
+require("core-js/modules/es.regexp.exec.js");
+
+require("core-js/modules/es.regexp.test.js");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.test = void 0;
+
+const test = () => {
+  console.log('===' + test);
+};
+
+exports.test = test;
