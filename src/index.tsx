3f174d8f65c21a08958a95a1a6f913d5de33b2a8
tien7668 <tien7668@gmail.com>
2021-11-16 10:21:26 +0700

    .

--- src/index.tsx
+++ src/index.tsx
@@ -19,9 +19,9 @@ import SEO from './components/SEO'
 
 const Web3ProviderNetwork = createWeb3ReactRoot(NetworkContextName)
 
-if ('ethereum' in window) {
-  ;(window.ethereum as any).autoRefreshOnNetworkChange = false
-}
+// if ('ethereum' in window) {
+//   ;(window.ethereum as any).autoRefreshOnNetworkChange = false
+// }
 
 function Updaters() {
   return (
